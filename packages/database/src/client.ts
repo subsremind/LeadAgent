d9b2433b2a2f79d20fe641f96fc0c8d3a72e1762
leon.ji <leon.ji@mspbots.ai>
2025-09-24 10:06:12 +0800

    change db client

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,14 +1,15 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 import * as schema from "../drizzle/schema";
- 
 
 if (!process.env.DATABASE_URL) {
 	throw new Error("DATABASE_URL is not set");
 }
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-});
-
+const queryClient = postgres(process.env.DATABASE_URL, {
+	ssl: { rejectUnauthorized: false },
+	max: 5,
+	idle_timeout: 20,
+	connect_timeout: 60,
+  });
  
-export const db = drizzle(pool, { schema });
+export const db = drizzle(queryClient, { schema });
